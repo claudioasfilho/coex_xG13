00eef4fc373ee5e8097f35c0d3410e517a40b5f8
Claudio Filho <claudioasfilho@gmail.com>
2020-03-19 10:31:42 -0700

    adding all files

---
+++ GNU ARM v7.2.1 - Default - EFR32BG13P632F512GM48/application_properties.d
@@ -0,0 +1,4 @@
+application_properties.o: ../application_properties.c \
+ /Users/clfilho/SimplicityStudio/NM_workspace/coex_xG13/platform/bootloader/api/application_properties.h
+
+/Users/clfilho/SimplicityStudio/NM_workspace/coex_xG13/platform/bootloader/api/application_properties.h:
---
+++ GNU ARM v7.2.1 - Default - EFR32BG13P632F512GM48/platform/emlib/src/em_assert.d
@@ -0,0 +1,4 @@
+platform/emlib/src/em_assert.o: ../platform/emlib/src/em_assert.c \
+ /Users/clfilho/SimplicityStudio/NM_workspace/coex_xG13/platform/emlib/inc/em_assert.h
+
+/Users/clfilho/SimplicityStudio/NM_workspace/coex_xG13/platform/emlib/inc/em_assert.h:
